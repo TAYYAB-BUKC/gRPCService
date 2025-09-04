c6e073dd7431da17cb5634a7cfec10c0e2485079
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-05 02:07:11 +0500

    ConsumeUnaryMethod implemented.

--- gRPCService.Client/Program.cs
+++ gRPCService.Client/Program.cs
@@ -2,8 +2,6 @@
 using Grpc.Net.Client;
 using gRPCService.Basics;
 
-Console.WriteLine("Hello, World!");
-
 string SERVER_URL = "https://localhost:7106/";
 var channelOptions = new GrpcChannelOptions()
 {
@@ -13,3 +11,14 @@ var channelOptions = new GrpcChannelOptions()
 using var channel = GrpcChannel.ForAddress(SERVER_URL, channelOptions);
 
 var client = new FirstGRPCServiceDefinition.FirstGRPCServiceDefinitionClient(channel);
+
+ConsumeUnaryMethod(client);
+
+Console.ReadKey(true);
+
+void ConsumeUnaryMethod(FirstGRPCServiceDefinition.FirstGRPCServiceDefinitionClient client)
+{
+	var request = new Request() { Content = "Hello gRPC Server" };
+	var response  = client.Unary(request);
+	Console.WriteLine(response.Message);
+}
