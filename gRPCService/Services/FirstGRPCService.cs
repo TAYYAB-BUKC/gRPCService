4fe7ead808957e2bdea2cbd64b5f441399de5cf9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-05 01:33:36 +0500

    Unary method implementation added.

--- gRPCService/Services/FirstGRPCService.cs
+++ gRPCService/Services/FirstGRPCService.cs
@@ -1,6 +1,16 @@
-﻿namespace gRPCService.Services
+﻿using Grpc.Core;
+using gRPCService.Basics;
+
+namespace gRPCService.Services
 {
-	public class FirstGRPCService
+	public class FirstGRPCService : FirstGRPCServiceDefinition.FirstGRPCServiceDefinitionBase
 	{
+		public override Task<Response> Unary(Request request, ServerCallContext context)
+		{
+			return Task.FromResult(new Response()
+			{
+				Message = $"Server received this content in the request: \n\n{request.Content}"
+			});
+		}
 	}
-}
+} 
