3a12303b324075aadf2b8a373c318b6eae231bc2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-05 01:59:30 +0500

    Configured channel options when creating a channel.

--- gRPCService.Client/Program.cs
+++ gRPCService.Client/Program.cs
@@ -4,5 +4,9 @@ using Grpc.Net.Client;
 Console.WriteLine("Hello, World!");
 
 string SERVER_URL = "https://localhost:7106/";
+var channelOptions = new GrpcChannelOptions()
+{
 
-using var channel = GrpcChannel.ForAddress(SERVER_URL);
+};
+
+using var channel = GrpcChannel.ForAddress(SERVER_URL, channelOptions);
