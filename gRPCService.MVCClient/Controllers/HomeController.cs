4cc638c24869203275970eed3317bb641d7d5ba4
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-05 03:35:53 +0500

    Consumed Unary method of gRPC implemented.

--- gRPCService.MVCClient/Controllers/HomeController.cs
+++ gRPCService.MVCClient/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using gRPCService.Basics;
 using gRPCService.MVCClient.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,14 +8,18 @@ namespace gRPCService.MVCClient.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly FirstGRPCServiceDefinition.FirstGRPCServiceDefinitionClient client;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, FirstGRPCServiceDefinition.FirstGRPCServiceDefinitionClient client)
 		{
 			_logger = logger;
+			this.client = client;
 		}
 
 		public IActionResult Index()
 		{
+			var response = client.Unary(new Request() { Content = "Hello from the gRPC MVC Client to the gRPC Server." });
+			ViewBag.Message = response.Message;
 			return View();
 		}
 
