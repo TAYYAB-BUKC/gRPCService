f281116eaa7f3072f947b9fbfa1f055b48143c6d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-08 02:10:32 +0500

    First integration test implemented.

---
+++ gRPCService.Tests.Integration/FirstGRPCServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using gRPCService.Basics;
+using gRPCService.Tests.Integration.Helpers;
+
+namespace gRPCService.Tests.Integration
+{
+	public class FirstGRPCServiceTests : IClassFixture<WebAppFactory<Program>>
+	{
+		private readonly WebAppFactory<Program> webAppFactory;
+
+		public FirstGRPCServiceTests(WebAppFactory<Program> webAppFactory)
+		{
+			this.webAppFactory = webAppFactory;
+		}
+
+		[Fact]
+		public async Task Unary_ShoulReturnMessage()
+		{
+			// Arrange
+			var client = webAppFactory.CreateGrpcClient();
+			var request = new Request()
+			{
+				Content = "Hello gRPC Server"
+			};
+			var expectedResponse = new Response()
+			{
+				Message = "Server(localhost) received this content in the request: \nHello gRPC Server"
+			};
+			
+			// Act
+			var actualResponse = client.Unary(request);
+
+			// Assert
+			actualResponse.Should().BeEquivalentTo(expectedResponse);
+		}
+	}
+}
--- gRPCService/Program.cs
+++ gRPCService/Program.cs
@@ -28,3 +28,5 @@ app.MapGrpcService<FirstGRPCService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+public partial class Program { }
