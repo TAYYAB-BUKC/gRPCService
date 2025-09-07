47777e4f35af1a6be12b0bdad17171bb13560c49
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-07 23:06:33 +0500

    TestServerCallContext implemented.

---
+++ gRPCService.Tests/Helpers/TestServerCallContext.cs
@@ -0,0 +1,45 @@
+﻿using Grpc.Core;
+
+namespace gRPCService.Tests.Helpers
+{
+	public class TestServerCallContext : ServerCallContext
+	{
+		private readonly Metadata metadata;
+		private readonly CancellationToken cancellationToken;
+
+		private TestServerCallContext(Metadata metadata, CancellationToken cancellationToken)
+		{
+			this.metadata = metadata;
+			this.cancellationToken = cancellationToken;
+		}
+
+		protected override string MethodCore => "Method Name";
+
+		protected override string HostCore => "Host Name";
+
+		protected override string PeerCore => "Peer Name";
+
+		protected override DateTime DeadlineCore { get; }
+
+		protected override Metadata RequestHeadersCore => throw new NotImplementedException();
+
+		protected override CancellationToken CancellationTokenCore => throw new NotImplementedException();
+
+		protected override Metadata ResponseTrailersCore => throw new NotImplementedException();
+
+		protected override Status StatusCore { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		protected override WriteOptions? WriteOptionsCore { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		protected override AuthContext AuthContextCore => throw new NotImplementedException();
+
+		protected override ContextPropagationToken CreatePropagationTokenCore(ContextPropagationOptions? options)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Task WriteResponseHeadersAsyncCore(Metadata responseHeaders)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
