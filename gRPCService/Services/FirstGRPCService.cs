6f03e9353f8b06b7bf2dfda53a60be63cffa1a80
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-07 22:12:23 +0500

    See retries in action on server side.

--- gRPCService/Services/FirstGRPCService.cs
+++ gRPCService/Services/FirstGRPCService.cs
@@ -9,6 +9,20 @@ namespace gRPCService.Services
 		public override Task<Response> Unary(Request request, ServerCallContext context)
 		{
 			context.WriteOptions = new WriteOptions(WriteFlags.NoCompress);
+			if(!context.RequestHeaders.Where(header => header.Key == "grpc-previous-rpc-attempts").Any())
+			{
+				throw new RpcException(new Status(StatusCode.Internal, "TRY AGAIN -_-"));
+			}
+
+			if (context.RequestHeaders.Where(header => header.Key == "grpc-previous-rpc-attempts").Any())
+			{
+				int value = Convert.ToInt32((context.RequestHeaders.Where(header => header.Key == "grpc-previous-rpc-attempts").FirstOrDefault().Value));
+				if (value < 3)
+				{
+					throw new RpcException(new Status(StatusCode.Internal, "TRY AGAIN -_-"));
+				}
+			}
+
 			return Task.FromResult(new Response()
 			{
 				Message = $"Server({context.Host}) received this content in the request: \n{request.Content}"
