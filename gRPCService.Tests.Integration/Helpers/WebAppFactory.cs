c55b8d2167c6a745962806f429d9c0dcec73f2a7
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-08 02:00:26 +0500

    WebAppFactory implemented.

---
+++ gRPCService.Tests.Integration/Helpers/WebAppFactory.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Net.Client;
+using gRPCService.Basics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace gRPCService.Tests.Integration.Helpers
+{
+	public class WebAppFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+	{
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			builder.UseEnvironment("gRPCService.Tests.Integration");
+			builder.ConfigureTestServices(services =>
+			{
+
+			});
+			builder.UseTestServer();
+		}
+
+		public FirstGRPCServiceDefinition.FirstGRPCServiceDefinitionClient CreateGrpcClient()
+		{
+			var httpClient = CreateClient();
+			var channel = GrpcChannel.ForAddress(httpClient.BaseAddress!, new GrpcChannelOptions()
+			{
+				HttpClient = httpClient
+			});
+
+			return new FirstGRPCServiceDefinition.FirstGRPCServiceDefinitionClient(channel);
+		}
+	}
+}
