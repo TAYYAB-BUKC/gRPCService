a0910504214a253a23aebe37115a7b15151a632c
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-07 17:19:34 +0500

    ClientLoggerInterceptor added in DI container.

--- gRPCService.MVCClient/Program.cs
+++ gRPCService.MVCClient/Program.cs
@@ -1,4 +1,5 @@
 using gRPCService.Basics;
+using gRPCService.MVCClient.GRPCInterceptors;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddGrpcClient<FirstGRPCServiceDefinition.FirstGRPCServiceDefinitionClient>(options =>
 {
 	options.Address = new Uri("https://localhost:7106/");
-});
+}).AddInterceptor<ClientLoggerInterceptor>();
+
+builder.Services.AddScoped<ClientLoggerInterceptor>();
 
 var app = builder.Build();
 
