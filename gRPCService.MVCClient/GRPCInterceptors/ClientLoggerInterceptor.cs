e2eaafaa95e9700f5db8898bd4f2139b319f04d4
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-07 17:07:13 +0500

    ClientLoggerInterceptor implemented.

---
+++ gRPCService.MVCClient/GRPCInterceptors/ClientLoggerInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core.Interceptors;
+
+namespace gRPCService.MVCClient.GRPCInterceptors
+{
+	public class ClientLoggerInterceptor : Interceptor
+	{
+		private readonly ILogger<ClientLoggerInterceptor> logger;
+		public ClientLoggerInterceptor(ILoggerFactory loggerFactory)
+		{
+			logger = loggerFactory.CreateLogger<ClientLoggerInterceptor>();
+		}
+
+		public override TResponse BlockingUnaryCall<TRequest, TResponse>(
+			TRequest request,
+			ClientInterceptorContext<TRequest, TResponse> context,
+			BlockingUnaryCallContinuation<TRequest, TResponse> continuation)
+		{
+			try
+			{
+				logger.LogInformation($"Starting the client call of type: {context.Method.FullName}, {context.Method.Type}");
+				return continuation(request, context);
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+		}
+	}
+}
