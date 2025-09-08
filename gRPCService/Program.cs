af1770326e4eee44a862de796dd0e04a3b7a2adf
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-08 23:53:01 +0500

    Server reflection implemented on server side.

--- gRPCService/Program.cs
+++ gRPCService/Program.cs
@@ -54,12 +54,15 @@ builder.Services.AddHealthChecks().AddCheck("gRPCService", () => HealthCheckResu
 builder.Services.AddGrpcHealthChecks();
 builder.Services.AddSingleton<HealthServiceImpl>();
 
+builder.Services.AddGrpcReflection();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<FirstGRPCService>();
 app.MapGrpcHealthChecksService();
+app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
