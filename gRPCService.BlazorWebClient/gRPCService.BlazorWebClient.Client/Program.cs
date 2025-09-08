74711ac38767e1e42a8916aee03e66af72d2e8a2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-09 00:31:49 +0500

    GrpcClient configuration implemented in Blazor client project.

--- gRPCService.BlazorWebClient/gRPCService.BlazorWebClient.Client/Program.cs
+++ gRPCService.BlazorWebClient/gRPCService.BlazorWebClient.Client/Program.cs
@@ -1,5 +1,15 @@
+using Grpc.Net.Client.Web;
+using gRPCService.Basics;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddGrpcClient<FirstGRPCServiceDefinition.FirstGRPCServiceDefinitionClient>(options =>
+{
+	options.Address = new Uri("https://localhost:7106/");
+}).ConfigureChannel(options =>
+{
+	options.HttpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+});
 
 await builder.Build().RunAsync();
